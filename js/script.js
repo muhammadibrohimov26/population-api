d9207fbaaefdd4474657bb156aa6f5c93d3de2e1
muhammad <muhammadibrohimov0306@gmail.com>
2025-01-25 17:12:37 +0500

    search

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-const isName = "uz";
 
-const apiUrl = `https://api.api-ninjas.com/v1/population?country=${isName}`;
-const apiKey = "xBaFxhJV205jIXuYqXP3cQ==2co1fd58tu29yXo1";
+const form = document.querySelector('form'),
+      input = form.querySelector('input'),
+      button = form.querySelector('button')
+      
+      button.addEventListener('click', (e)=>{
+        
+        e.preventDefault()
+        const isName  = input.value
+        input.value = ''
+        if(isName.trim() === ''){
+          console.log('xatooo');
+      
+        }
+        
+        
+        const apiKey = "xBaFxhJV205jIXuYqXP3cQ==2co1fd58tu29yXo1";
+        const apiUrl = `https://api.api-ninjas.com/v1/population?country=${isName}`;
 
 fetch(apiUrl, {
   method: "GET",
@@ -14,6 +28,7 @@ fetch(apiUrl, {
     console.log(data);
     country(data);
   });
+})
 
 function country(data) {
   const header = document.querySelector(".header");
@@ -23,10 +38,12 @@ function country(data) {
   const tarixiy = data.historical_population;
 
   tarixiy.forEach((item, idx) => {
+    console.log(idx);
+    
     const cardItem = document.createElement("div");
     cardItem.classList.add("cardItem");
-    const year = document.createElement("p");
-    const population = document.createElement("span");
+    const year = document.createElement("h2");
+    const population = document.createElement("p");
     const yearlychangepercentage = document.createElement("p");
     const yearlychange = document.createElement("p");
     const migrants = document.createElement("p");
@@ -37,23 +54,37 @@ function country(data) {
     const urbanpopulation = document.createElement("p");
     const percentageofworldpopulation = document.createElement("p");
     const rank = document.createElement("p");
+    const index = document.createElement("span");
 
-    year.textContent = item.year + " Year";
-    population.textContent = item.population + " population";
-    yearlychangepercentage.textContent = item.yearly_change_percentage + " yearlychangepercentage";
-    yearlychange.textContent = item.yearly_change + " yearlychange";
-    // year.textContent = item. + " ";
-    // year.textContent = item. + " ";
-    // year.textContent = item. + " ";
-    // year.textContent = item. + " ";
-    // year.textContent = item. + " ";
-    // year.textContent = item. + " ";
-    // year.textContent = item. + " ";
+    year.textContent = item.year + " --      Year";
+    population.textContent = item.population + " --      population";
+    yearlychangepercentage.textContent = item.yearly_change_percentage + "%" + " --      yearly change percentage";
+    yearlychange.textContent = item.yearly_change + " --      yearly change";
+    migrants.textContent = item.migrants + ' --      migrants'
+    medianage.textContent = item.median_age + " --      median Age";
+    fertilityrate.textContent = item.fertility_rate + " --      fertility rate";
+    density.textContent = item.density + " --      density";
+    urbanpopulationpct.textContent = item.urban_population_pct  + " --      urban population pct";
+    urbanpopulation.textContent = item.urban_population + " --      urban population";
+    percentageofworldpopulation.textContent = item.percentage_of_world_population + " --      percentage of world population";
+    rank.textContent = item.rank + " --      rank";
+    index.textContent = idx +1;
+    // .textContent = item. + " ";
 
     cardItem.append(year);
     cardItem.append(population);
     cardItem.append(yearlychangepercentage);
     cardItem.append(yearlychange);
+    cardItem.append(migrants)
+    cardItem.append(index)
+    cardItem.append(medianage)
+    cardItem.append(fertilityrate)
+    cardItem.append(density)
+    cardItem.append(urbanpopulationpct)
+    cardItem.append(urbanpopulation)
+    cardItem.append(percentageofworldpopulation)
+    cardItem.append(rank)
+    // cardItem.append()
     cardContent.append(cardItem);
   });
 }
